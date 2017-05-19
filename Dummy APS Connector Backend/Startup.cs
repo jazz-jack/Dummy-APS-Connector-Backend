11468c21fa339764326ec4057bc6d76a8f803677
Mike Salnikov <msalnikov@odin.com>
2017-05-19 19:26:05 +0300

    support for YAML config files

--- Dummy APS Connector Backend/Startup.cs
+++ Dummy APS Connector Backend/Startup.cs
@@ -1,22 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace Dummy_APS_Connector_Backend
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public Startup(IHostingEnvironment env)
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddEnvironmentVariables()
+                .AddYamlFile(Environment.GetEnvironmentVariable("CONFIG_FILE"));
+            Configuration = builder.Build();
         }
+
+        public IConfigurationRoot Configuration { get; }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
@@ -27,10 +30,18 @@ namespace Dummy_APS_Connector_Backend
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.Map("/config", HandleMapConfig);
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                context.Response.ContentType = "application/json";
+                await context.Response.WriteAsync("{}");
+            });
+        }
+        private void HandleMapConfig(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync($"my_special_option_for_aps_connect = {Configuration["my_special_option_for_aps_connect"]}");
             });
         }
     }
